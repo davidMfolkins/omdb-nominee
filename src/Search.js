bcda9646aa7314f8d9066fe0f55348ef8ec3bb60
David Folkins <david.folkins88@gmail.com>
2021-01-11 10:35:26 -0500

    assign value to what is typed in search

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,15 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function Search() {
+  const [value, setValue] = useState(""); 
 
   useEffect(() => {
-    const URL = `https://omdbapi.com/?apikey=a086c7ae&t=blade+runner`;
+    const URL = `https://omdbapi.com/?apikey=a086c7ae&t=${value}`;
     axios.get(URL).then(response => {
       console.log(response)
     });
-  }, [])
+  }, [value])
 
   return (
     <div className="searchBarContainer">
@@ -18,8 +19,11 @@ function Search() {
           name="search"
           placeholder="Search for an movie..."
           spellcheck="false"
+          value={value}
+          onChange={event => setValue(event.target.value)}
         />
       </form>
+      <div></div>
     </div>
   );
 }
