8d2eea980145f1389a961bf0b9df3f0c86808f66
David Folkins <david.folkins88@gmail.com>
2021-01-11 10:29:51 -0500

    add test search for api

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,15 @@
-
+import { useEffect } from 'react'
+import axios from 'axios';
 
 function Search() {
+
+  useEffect(() => {
+    const URL = `https://omdbapi.com/?apikey=a086c7ae&t=blade+runner`;
+    axios.get(URL).then(response => {
+      console.log(response)
+    });
+  }, [])
+
   return (
     <div className="searchBarContainer">
       <form onSubmit={event => event.preventDefault()}>
