d9c727e524206aca57631f7ba905516c6df0cdd1
David Folkins <david.folkins88@gmail.com>
2021-01-11 11:37:01 -0500

    attempting to have results display

--- src/Search.js
+++ src/Search.js
@@ -2,14 +2,20 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function Search() {
-  const [value, setValue] = useState(""); 
+  const [value, setValue] = useState("");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const URL = `https://omdbapi.com/?apikey=a086c7ae&t=${value}`;
     axios.get(URL).then(response => {
-      console.log(response)
+      console.log(response.data)
+      setResults(response.data.title)
     });
   }, [value])
+  
+  const result = results && results.map(movie => {
+    return <p>{movie.title}</p>
+  })
 
   return (
     <div className="searchBarContainer">
@@ -23,7 +29,7 @@ function Search() {
           onChange={event => setValue(event.target.value)}
         />
       </form>
-      <div></div>
+      <div>{result}</div>
     </div>
   );
 }
