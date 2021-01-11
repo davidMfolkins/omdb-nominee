7fa83516d990e0d7d6f485f755cb4b7b3ca645e6
David Folkins <david.folkins88@gmail.com>
2021-01-11 13:28:15 -0500

    Add more css to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Search from './Search'
 function App() {
   return (
     <div className="App">
-      <div>The Shoppies</div>
+      <div className="app-title">The Shoppies</div>
       <Search />
     </div>
   );
