c35c32ae7a1609fdf36460e4be53c20d4b97ca92
David Folkins <david.folkins88@gmail.com>
2021-01-15 10:24:09 -0500

    Add screenshots for readme

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="app-title">The Shoppies!</div>
+      <div className="sub-title">Movie awards for entrepreneurs</div>
       <Search />
       <div className="footer-container">
         <div className="footer">Made by <a href="https://github.com/davidMfolkins" target="_blank">David</a></div>
